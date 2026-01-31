da70152ed160cb0e1bd2a1dae9226edfde27b1a5
fxxsj <i@fxxsj.eu.org>
2026-01-31 10:40:46 +0800

    feat(03-01): add preview method to taskAssignmentRuleAPI
    - Added preview() method to TaskAssignmentRuleAPI class
    - Calls GET /task-assignment-rules/preview/ endpoint
    - Returns preview data with process-department mappings
    - Extends BaseAPI with custom preview functionality

--- src/api/modules/task-assignment-rule.js
+++ src/api/modules/task-assignment-rule.js
@@ -9,6 +9,20 @@ class TaskAssignmentRuleAPI extends BaseAPI {
   constructor() {
     super('/task-assignment-rules/', request)
   }
+
+  /**
+   * 获取分派预览
+   * 返回所有工序的分派效果预览
+   * @param {Object} params - 查询参数
+   * @returns {Promise} 预览数据
+   */
+  preview(params = {}) {
+    return this.request({
+      url: `${this.baseURL}preview/`,
+      method: 'get',
+      params
+    })
+  }
 }
 
 export const taskAssignmentRuleAPI = new TaskAssignmentRuleAPI()
