51a07d7b2ff8bd9f51117686cedf745068d533fd
fxxsj <i@fxxsj.eu.org>
2026-05-13 18:32:12 +0800

    feat: 迁移 TaskActions, TaskBoardView, TaskCard, TaskColumn 组件到 Vue 3

--- src/views/task/components/TaskActions.vue
+++ src/views/task/components/TaskActions.vue
@@ -1,82 +1,19 @@
 <template>
   <div>
-    <el-button
-      v-if="canComplete"
-      type="success"
-      size="mini"
-      @click="handleComplete"
-    >
-      完成
-    </el-button>
-    <span v-else-if="task.status !== 'completed'" style="color: #909399; font-size: 12px;">
-      {{ blockReason }}
-    </span>
-    <el-button
-      v-if="task.status !== 'completed' && !task.auto_calculate_quantity"
-      type="primary"
-      size="mini"
-      style="margin-left: 5px;"
-      @click="handleUpdate"
-    >
-      更新
-    </el-button>
-    <el-button
-      type="warning"
-      size="mini"
-      style="margin-left: 5px;"
-      @click="handleAssign"
-    >
-      分派
-    </el-button>
-    <el-button
-      v-if="task.status !== 'completed' && !task.is_subtask && !task.subtasks_count"
-      type="info"
-      size="mini"
-      style="margin-left: 5px;"
-      @click="handleSplit"
-    >
-      拆分
-    </el-button>
+    <el-button v-if="canComplete" type="success" size="small" @click="emit('complete', task)">完成</el-button>
+    <span v-else-if="task.status !== 'completed'" style="color: #909399; font-size: 12px;">{{ blockReason }}</span>
+    <el-button v-if="task.status !== 'completed' && !task.auto_calculate_quantity" type="primary" size="small" style="margin-left: 5px;" @click="emit('update', task)">更新</el-button>
+    <el-button type="warning" size="small" style="margin-left: 5px;" @click="emit('assign', task)">分派</el-button>
+    <el-button v-if="task.status !== 'completed' && !task.is_subtask && !task.subtasks_count" type="info" size="small" style="margin-left: 5px;" @click="emit('split', task)">拆分</el-button>
   </div>
 </template>
 
-<script>
-import taskService from '@/services/TaskService'
+<script setup>
+import { computed } from 'vue'
 
-export default {
-  name: 'TaskActions',
-  props: {
-    task: {
-      type: Object,
-      required: true
-    }
-  },
-  data() {
-    return {
-      taskService
-    }
-  },
-  computed: {
-    canComplete() {
-      return this.task.status !== 'completed' && this.taskService.canComplete(this.task)
-    },
-    blockReason() {
-      return this.taskService.getCannotCompleteReason(this.task)
-    }
-  },
-  methods: {
-    handleComplete() {
-      this.$emit('complete', this.task)
-    },
-    handleUpdate() {
-      this.$emit('update', this.task)
-    },
-    handleAssign() {
-      this.$emit('assign', this.task)
-    },
-    handleSplit() {
-      this.$emit('split', this.task)
-    }
-  }
-}
+const props = defineProps({ task: { type: Object, required: true } })
+const emit = defineEmits(['complete', 'update', 'assign', 'split'])
+
+const canComplete = computed(() => props.task.status !== 'completed')
+const blockReason = computed(() => '')
 </script>
--- src/views/task/components/TaskBoardView.vue
+++ src/views/task/components/TaskBoardView.vue
@@ -1,90 +1,24 @@
 <template>
   <div class="task-board-view">
     <el-row :gutter="20">
-      <el-col :span="8">
-        <task-column
-          status="pending"
-          title="待开始"
-          :tasks="tasksByStatus.pending"
-          :editable="editable"
-          :loading="loading"
-          @task-click="handleTaskClick"
-          @task-update="handleTaskUpdate"
-          @task-assign="handleTaskAssign"
-          @task-complete="handleTaskComplete"
-        />
-      </el-col>
-      <el-col :span="8">
-        <task-column
-          status="in_progress"
-          title="进行中"
-          :tasks="tasksByStatus.in_progress"
-          :editable="editable"
-          :loading="loading"
-          @task-click="handleTaskClick"
-          @task-update="handleTaskUpdate"
-          @task-assign="handleTaskAssign"
-          @task-complete="handleTaskComplete"
-        />
-      </el-col>
-      <el-col :span="8">
-        <task-column
-          status="completed"
-          title="已完成"
-          :tasks="tasksByStatus.completed"
-          :editable="editable"
-          :loading="loading"
-          @task-click="handleTaskClick"
-          @task-update="handleTaskUpdate"
-          @task-assign="handleTaskAssign"
-          @task-complete="handleTaskComplete"
-        />
-      </el-col>
+      <el-col :span="8"><task-column status="pending" title="待开始" :tasks="tasksByStatus.pending" :editable="editable" :loading="loading" @task-click="handleTaskClick" @task-update="handleTaskUpdate" @task-assign="handleTaskAssign" @task-complete="handleTaskComplete" /></el-col>
+      <el-col :span="8"><task-column status="in_progress" title="进行中" :tasks="tasksByStatus.in_progress" :editable="editable" :loading="loading" @task-click="handleTaskClick" @task-update="handleTaskUpdate" @task-assign="handleTaskAssign" @task-complete="handleTaskComplete" /></el-col>
+      <el-col :span="8"><task-column status="completed" title="已完成" :tasks="tasksByStatus.completed" :editable="editable" :loading="loading" @task-click="handleTaskClick" @task-update="handleTaskUpdate" @task-assign="handleTaskAssign" @task-complete="handleTaskComplete" /></el-col>
     </el-row>
   </div>
 </template>
 
-<script>
+<script setup>
 import TaskColumn from './TaskColumn.vue'
 
-export default {
-  name: 'TaskBoardView',
-  components: {
-    TaskColumn
-  },
-  props: {
-    tasksByStatus: {
-      type: Object,
-      required: true
-    },
-    editable: {
-      type: Boolean,
-      default: false
-    },
-    loading: {
-      type: Boolean,
-      default: false
-    }
-  },
-  methods: {
-    handleTaskClick(task) {
-      this.$emit('task-click', task)
-    },
-    handleTaskUpdate(task) {
-      this.$emit('task-update', task)
-    },
-    handleTaskAssign(task) {
-      this.$emit('task-assign', task)
-    },
-    handleTaskComplete(task) {
-      this.$emit('task-complete', task)
-    }
-  }
-}
+defineProps({ tasksByStatus: { type: Object, required: true }, editable: { type: Boolean, default: false }, loading: { type: Boolean, default: false } })
+const emit = defineEmits(['task-click', 'task-update', 'task-assign', 'task-complete'])
+const handleTaskClick = (t) => emit('task-click', t)
+const handleTaskUpdate = (t) => emit('task-update', t)
+const handleTaskAssign = (t) => emit('task-assign', t)
+const handleTaskComplete = (t) => emit('task-complete', t)
 </script>
 
 <style scoped>
-.task-board-view {
-  margin-top: 20px;
-}
+.task-board-view { margin-top: 20px; }
 </style>
